adc1dc00eb6c746bd6c1167c652b430223904bb4
liljdot <jehuoki@gmail.com>
2025-01-02 16:28:51 +0100

    added path

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const cors = require("cors")
+const path = require("path")
 
 const app = express()
 
 app.use(cors())
-app.use(express.static("pages"))
+app.use(express.static(path.join(__dirname,"pages")))
 
 app.get('/hello', (req, res) => {
     res.send("<h1>hello</h1>")
