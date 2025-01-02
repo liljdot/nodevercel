8434070e5d46945b2fb605070f2bb6e573909203
liljdot <jehuoki@gmail.com>
2025-01-02 16:19:58 +0100

    add test route

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const app = express()
 app.use(cors())
 app.use(express.static("pages"))
 
+app.get('/hello', (req, res) => {
+    res.send("<h1>hello</h1>")
+})
+
 app.listen(process.env.PORT || 3000, () => {
     console.log("server is running")
     console.log()
