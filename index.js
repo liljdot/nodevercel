24cf5b4a964b1e5d8aa9a46e603032bb4358ea47
liljdot <jehuoki@gmail.com>
2025-01-02 16:17:00 +0100

    add cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express")
+const cors = require("cors")
 
 const app = express()
 
+app.use(cors())
 app.use(express.static("pages"))
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("server is running")
+    console.log()
 })
